e24521382cea2f63931a61c7393e8dec20b293d1
Djadimadje <m.djadimadje@gmail.com>
2025-03-02 06:39:03 -0800

    modify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # Make sure this import is correct
 
 # Function-based view to list all books
 def list_books(request):
     """
     Lists all books stored in the database with their authors.
     """
-    books = Book.objects.all().select_related('author')  # Optimize to include author data
+    books = Book.objects.select_related('author').all()  # Efficiently fetch related authors
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to display details for a specific library
@@ -15,14 +15,15 @@ class LibraryDetailView(DetailView):
     """
     Displays details for a specific library, including all books available in that library.
     """
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    model = Library  # The model associated with this view
+    template_name = 'relationship_app/library_detail.html'  # Template to render
+    context_object_name = 'library'  # Context variable name in the template
 
     def get_context_data(self, **kwargs):
         """
         Adds additional context data if needed.
         """
         context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()  # List all books in the library
+        context['books'] = self.object.books.all()  # Add the books related to the library
         return context
+
