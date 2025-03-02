23016080b50496071f7e764390ca752432657036
Djadimadje <m.djadimadje@gmail.com>
2025-03-02 06:53:12 -0800

    modify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import DetailView
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from .views import list_books
 
 # Function-based view to list all books
 def list_books(request):
