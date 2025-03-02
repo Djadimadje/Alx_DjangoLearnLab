fd1b3ea8fb0eb86129f691215b9b6bf091a9c441
Djadimadje <m.djadimadje@gmail.com>
2025-03-02 07:25:20 -0800

    Modify urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,8 +11,13 @@ urlpatterns = [
     # Class-based view for library details
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     # Custom login view using a custom template
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    
+    # Custom logout view using a custom template
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    
+    # Registration view (custom view for user registration)
     path('register/', views.register, name='register'),
 ]
 
