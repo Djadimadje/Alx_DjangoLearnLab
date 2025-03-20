e20f13ca1ccbc46642a0191479a847931f280b5b
Djadimadje <m.djadimadje@gmail.com>
2025-03-20 06:31:34 -0700

    Modify urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),  # Homepage displaying all posts
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),  # View a single post
     path("post/new/", PostCreateView.as_view(), name="post-create"),  # Create a new post
-    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),  # Edit a post
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),  # Edit a post
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),  # Delete a post
 
     # Password Reset URLs (Optional)
