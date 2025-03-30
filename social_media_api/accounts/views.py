163d832f14aa72fbf0ec9aaaf930f846df71e8eb
Mrangaye Djadimadje <130258651+Djadimadje@users.noreply.github.com>
2025-03-30 18:38:21 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny  # Explicitly import both
+from rest_framework.permissions import AllowAny 
+from rest_framework.permissions import IsAuthenticated # Explicitly import both
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from rest_framework import status
